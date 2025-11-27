e20420eb674ab5b17d87a0c3cec0841a1c915d74
kimdaechul <rlaeocjf@gmail.com>
2025-11-27 19:01:27 +0900

    message

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,15 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './component/Header'
 import TodoEditor from './component/TodoEditor'
 import TodoList from './component/TodoList'
 
-function App() {
-  
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createDate: new Date().getTime(),
+  },
+]
 
+function App() {
+    
+  const [todo, setTodo] = useState(mockTodo);
+  
+  /* 데이타 추가 하기*/
+  const onCreate = (content) => {
+    const newItem = {
+      id: 0,
+      isDone: false,
+      content,
+      createDate: new Date().getTime(),
+    }
+    setTodo([...todo, newItem]);
+  };
+  
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor  onCreate = {onCreate}/>
       <TodoList />
     </div>
   )
--- project2/src/component/TodoEditor.jsx
+++ project2/src/component/TodoEditor.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+const TodoEditor = ({onCreate}) => {
+
+    const [content, setContent] = useState("");
+
+    const onChangeContent = (e)=>{
+        setContent(e.target.value);
+    }
+
+
     return (
         <div className='TodoEditor'>
             <h4>새로운 Todo 작성하기 </h4>
             <div className='editor_wrapper'>
-                <input placeholder='새로운 Todo...' />
+                <input 
+                onChange={onChangeContent}
+                placeholder='새로운 Todo...' />
+                
                 <button>추가</button>
             </div>
         </div>
