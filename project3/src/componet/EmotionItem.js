ceff0127b428bebf89ed5dd8a6bc7f8437ac546f
kimdaechul <rlaeocjf@gmail.com>
2025-12-04 18:51:57 +0900

    message

--- project3/src/componet/EmotionItem.js
+++ project3/src/componet/EmotionItem.js
@@ -8,7 +8,8 @@ const EmotionItem = ({id, img, name, onClick, isSelected}) => {
     };
 
     return (
-        <div className='EmotionItem' onClick={handleOnClick}>
+        <div className={ ['EmotionItem', isSelected ? `EmotionItem_on_${id}` : `EmotionItem_off`].join(" ") } 
+                onClick={handleOnClick} >
             <img alt={`emotion${id}`} src={img} />
             <span>{name}</span>
         </div>
